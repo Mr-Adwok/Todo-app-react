93e20dc8e5149db873e37509126bb704cf95edc0
Mr-Adwok <adwokbrave@gmail.com>
2026-04-08 15:23:52 +0200

    Add: first component App

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+// creating the first component 
+
+
+export default function App(){
+
+  return( 
+        <>
+          <h1 className ="htodos"> todos </h1>
+        </>
+           ) 
+
+
+}
